f4f6dd7af2c33d838669193f1afad0986530c9e2
hypc <h_yp00@163.com>
2018-02-03 13:26:35 +0800

    bg-nest

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    website: {
+        assets: 'assets',
+        js: [
+            'bg-nest.min.js',
+            'init.js'
+        ]
+    }
+}
---
+++ src/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp node_modules/bg-nest/dist/*.js assets/
+cp src/init.js assets/
